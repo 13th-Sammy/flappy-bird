87794c3c616488d62b2baf96dcd5075b22131879
13th-Sammy <singhpalu732@gmail.com>
2025-05-31 00:48:45 +0530

    background

--- main.cpp
+++ main.cpp
@@ -7,11 +7,14 @@ using std::cin;
 using std::endl;
 int main()
 {
-    Texture birdTexture;
+    Texture birdTexture, backgroundTexture;
     birdTexture.loadFromFile("assets/bird.png");  //load bird texture
     Sprite bird;
     bird.setTexture(birdTexture);
     bird.setPosition(300, 450); //set bird texture and initial position to sprite
+    backgroundTexture.loadFromFile("assets/background.png"); //load background texture
+    Sprite background;
+    background.setTexture(backgroundTexture); //set background texture to sprite
     float birdVelocity = 0.25; //downwards bird velocity
     //Create a Game Window
     RenderWindow window(VideoMode(1600, 900), "Flappy Bird");
@@ -33,6 +36,7 @@ int main()
         bird.setPosition(bird.getPosition().x, bird.getPosition().y + birdVelocity); // position of bird goes down every frame
         //DRAW EVERYTHING ON SCREEN, CONTINUOUSLY -
         window.clear(); //clear window every frame
+        window.draw(background);
         window.draw(bird); //draw bird, all drawing must be between window.clear and display
         window.display(); // swap front and back buffers, continuity between frames thing
     }
